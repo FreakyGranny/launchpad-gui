ec47b56cda2cf5534e478bb90ceaaf2e0762eec3
FreakyGranny <mora378@rambler.ru>
2019-10-19 19:14:30 +0700

    first working prototype

---
+++ src/main.js
@@ -0,0 +1,54 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+import auth from "./auth";
+import App from "./components/App.vue";
+import About from "./components/About.vue";
+import Dashboard from "./components/Dashboard.vue";
+import Login from "./components/Login.vue";
+
+Vue.config.productionTip = false;
+
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
+Vue.use(VueMaterial);
+
+function requireAuth(to, from, next) {
+  if (!auth.loggedIn()) {
+    next({
+      path: "/login",
+      query: { redirect: to.fullPath }
+    });
+  } else {
+    next();
+  }
+}
+
+const router = new VueRouter({
+  mode: "history",
+  base: __dirname,
+  routes: [
+    { path: "/about", component: About },
+    { path: "/dashboard", component: Dashboard, beforeEnter: requireAuth },
+    { path: "/login", component: Login },
+    {
+      path: "/logout",
+      beforeEnter(to, from, next) {
+        auth.logout();
+        next("/");
+      }
+    }
+  ]
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  router,
+  // replace the content of <div id="app"></div> with App
+  render: h => h(App)
+});
