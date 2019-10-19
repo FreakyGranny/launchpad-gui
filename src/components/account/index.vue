9fb7521d9444917b18bdf7433e05a54e8e77da25
p.fomin <p.fomin@2gis.ru>
2019-10-19 22:48:26 +0700

    working auth

---
+++ src/components/account/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="headline">
+      <h1>Your doge profile</h1>
+    </div>
+    <p v-if="profile.username">
+      <strong>Name:</strong> {{ profile.username }} {{ profile.avatar }}
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.headline {
+  margin-top: 1em;
+  display: flex;
+  img {
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+  }
+}
+</style>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "account",
+  computed: mapState({ profile: state => state.user.profile })
+};
+</script>
