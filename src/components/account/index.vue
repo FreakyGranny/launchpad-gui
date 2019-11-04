b49c6aa6f9c16771540feb4767e73d97ab4febec
FreakyGranny <mora378@rambler.ru>
2019-11-04 18:32:51 +0700

    adding profile page

--- src/components/account/index.vue
+++ src/components/account/index.vue
@@ -1,27 +1,168 @@
 <template>
   <div>
-    <div class="header header-account">
-      <span class="white-text">Профиль</span>
+    <load-spinner v-if="!IS_PROFILE_LOADED" />
+    <user-card :user="PROFILE" v-if="IS_PROFILE_LOADED" />
+    <div
+      v-if="IS_PROFILE_LOADED"
+      class="md-layout projects-grid md-alignment-top-center"
+    >
+      <md-tabs md-alignment="fixed">
+        <template slot="md-tab" slot-scope="{ tab }">
+          <span class="md-subheading tab-head">{{ tab.label }} </span>
+          <md-chip v-if="tab.data.badge">{{ tab.data.badge }} </md-chip>
+        </template>
+
+        <md-tab
+          id="tab-owned"
+          md-label="Я автор"
+          :md-template-data="{ badge: projectsOwned.length }"
+        >
+          <md-content class="tab-content">
+            <load-spinner v-if="loading" />
+            <div class="md-layout md-alignment-top-center">
+              <div class="spacing" v-if="!loading">
+                <md-empty-state
+                  md-icon="help_outline"
+                  md-label="Проекты не найдены"
+                  md-description="Вы пока не являетесь автором ни одного проекта."
+                  v-if="projectsOwned.length == 0"
+                />
+              </div>
+              <project-card
+                v-for="(project, index) in projectsOwned"
+                :key="index"
+                :project="project"
+                :category="CATEGORY[project.category[0]]"
+              />
+            </div>
+          </md-content>
+        </md-tab>
+
+        <md-tab
+          id="tab-member"
+          md-label="Я участник"
+          :md-template-data="{ badge: projectsContributed.length }"
+        >
+          <md-content class="tab-content">
+            <div class="md-layout md-alignment-top-center">
+              <div class="spacing" v-if="!loading">
+                <md-empty-state
+                  md-icon="help_outline"
+                  md-label="Проекты не найдены"
+                  md-description="Вы пока не поддержали ни один проект."
+                  v-if="projectsContributed.length == 0"
+                />
+              </div>
+              <project-card
+                v-for="(project, index) in projectsContributed"
+                :key="index"
+                :project="project"
+                :category="CATEGORY[project.category[0]]"
+              />
+            </div>
+          </md-content>
+        </md-tab>
+
+        <md-tab
+          id="tab-likes"
+          md-label="Мне понравилось"
+          :md-template-data="{ badge: '0' }"
+          md-disabled
+        >
+          <md-content class="tab-content"> </md-content>
+        </md-tab>
+      </md-tabs>
     </div>
-    <p v-if="IS_PROFILE_LOADED">
-      <strong>Name:</strong> {{ PROFILE.first_name }} {{ PROFILE.last_name }}
-    </p>
   </div>
 </template>
 
 <style scoped>
 .header-account {
-  background-image: url("/images/keys.jpg");
+  background-image: url("/images/user.jpg");
+  height: 300px;
+  line-height: 300px;
+}
+.projects-grid {
+  padding-top: 30px;
+  margin: auto;
+  max-width: 1500px;
+}
+.md-chip {
+  margin-left: 10px;
+  height: 20px;
+  line-height: 20px;
 }
 </style>
 
 <script>
+import ProjectCard from "../lib/ProjectCard";
+import UserCard from "../lib/UserCard";
+import LoadSpinner from "../lib/loading";
+import axios from "axios";
 import { mapGetters } from "vuex";
 
+export const FILTER_OWNED = "owned";
+export const FILTER_CONTRIBUTED = "contributed";
+
 export default {
+  components: {
+    ProjectCard,
+    UserCard,
+    LoadSpinner
+  },
   name: "account",
+  created: async function() {
+    this.getProjects(FILTER_OWNED);
+    this.getProjects(FILTER_CONTRIBUTED);
+  },
   computed: {
-    ...mapGetters(["IS_PROFILE_LOADED", "PROFILE"])
+    ...mapGetters([
+      "STORAGE_STATUS",
+      "IS_CATEGORY_LOADED",
+      "IS_PROJECT_TYPE_LOADED",
+      "IS_PROFILE_LOADED",
+      "CATEGORY",
+      "PROJECT_TYPE",
+      "PROFILE"
+    ])
+  },
+  methods: {
+    buildProjectUrl(filter) {
+      let pUrl = "/project?page_size=20";
+      if (filter === FILTER_OWNED) {
+        pUrl += "&filter=" + FILTER_OWNED;
+      }
+      if (filter === FILTER_CONTRIBUTED) {
+        pUrl += "&filter=" + FILTER_CONTRIBUTED;
+      }
+      return pUrl;
+    },
+    getProjects(filter) {
+      this.loading = true;
+      axios({ url: this.buildProjectUrl(filter) })
+        .then(resp => {
+          this.loading = false;
+          if (filter == FILTER_OWNED) {
+            this.projectsOwned = resp.data.results;
+            // window.console.log(resp);
+          }
+          if (filter == FILTER_CONTRIBUTED) {
+            this.projectsContributed = resp.data.results;
+          }
+        })
+        .catch(resp => {
+          this.error = resp.data;
+          this.loading = false;
+        });
+    }
+  },
+  data() {
+    return {
+      loading: true,
+      error: null,
+      projectsOwned: [],
+      projectsContributed: []
+    };
   }
 };
 </script>
---
+++ src/components/lib/UserCard.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="outer">
+    <div class="user-header">
+      <div class="md-layout md-alignment-center inner">
+        <md-content class="md-elevation-12 user-card">
+          <div class="card-content md-layout md-alignment-top-left">
+            <div class="md-layout-item md-size-40">
+              <md-avatar class="avatar-custom">
+                <img
+                  v-bind:src="user.avatar"
+                  onerror="this.onerror=null;this.src='/images/avatar.jpeg';"
+                  alt="Avatar"
+                />
+              </md-avatar>
+            </div>
+            <div class="md-layout-item md-size=60">
+              <div class="md-display-1">
+                {{ user.first_name }} {{ user.last_name }}
+              </div>
+              <md-divider class="divider-space" />
+              <div class="md-layout">
+                <div class="md-layout-item md-size-100 card-line">
+                  <div class="md-subheading">Проектов</div>
+                  <div class="md-subheading counter">
+                    {{ user.profile.project_count }}
+                  </div>
+                </div>
+              </div>
+              <div class="md-layout bottom-link-bar">
+                <md-button
+                  :href="'https://planeta.2gis.ru/profile/' + user.username"
+                  class="md-icon-button md-primary"
+                >
+                  <md-icon>language</md-icon>
+                  <md-tooltip md-direction="bottom"
+                    >Открыть профиль на планете</md-tooltip
+                  >
+                </md-button>
+              </div>
+            </div>
+          </div>
+          <md-content class="bottom-bar md-primary" />
+        </md-content>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.outer {
+  height: 400px;
+}
+.user-header {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/keys.jpg");
+  height: 300px;
+  position: relative;
+}
+.inner {
+  width: 100%;
+  position: absolute;
+  top: 60px;
+}
+.user-card {
+  width: 800px;
+  display: inline-block;
+  vertical-align: top;
+}
+.card-content {
+  margin: 30px;
+}
+.bottom-bar {
+  height: 7px;
+}
+.divider-space {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.avatar-custom {
+  min-width: 250px;
+  min-height: 250px;
+  border-radius: 0px;
+}
+.counter {
+  font-weight: 700;
+  padding-left: 1%;
+}
+.card-line {
+  margin-top: 5px;
+  padding-left: 15px;
+  display: inline-flex;
+}
+.bottom-link-bar {
+  margin-top: 120px;
+}
+</style>
+
+<script>
+export default {
+  name: "UserCard",
+  data() {
+    return {};
+  },
+  props: {
+    user: Object
+  }
+};
+</script>
